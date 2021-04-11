0255f5b16bd9c4ec2e884be556b962c39d5b591b
yuks <yuks@asiainfo.com>
2021-04-11 12:34:10 +0800

    工具类更新

--- java/src/main/java/com/web/util/DateUtil.java
+++ java/src/main/java/com/web/util/DateUtil.java
@@ -6,14 +6,30 @@ import java.util.Date;
 
 public class DateUtil {
     /**
-     * 时间转换成时间戳
+     * Date转换格式化时间
      * @param currentTime 传入的时间
-     * @return 返回时间戳
+     * @return 格式化时间
      * @throws ParseException
      */
-    public static String timeToTimeStamp(Date currentTime) {
+    public static String DateToFormatTime(Date currentTime) {
         try {
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return formatter.format(currentTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * Date转换格式化时间
+     * @param currentTime 传入的时间
+     * @return 格式化时间
+     * @throws ParseException
+     */
+    public static String DateToFormatTime(Date currentTime,String format) {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat(format);
             return formatter.format(currentTime);
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,6 +56,35 @@ public class DateUtil {
         return res;
     }
 
+    /**
+     * 字符时间转换成时间
+     * @param s 传入的时间字符
+     * @return 返回格式化时间
+     */
+    public static Date timeStringToDate(String s){
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss");
+            return formatter.parse(s);
+        } catch (NumberFormatException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 字符时间转换成时间
+     * @param s 传入的时间字符 format 指定格式
+     * @return 返回格式化时间
+     */
+    public static Date timeStringToDate(String s,String format){
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat( format);
+            return formatter.parse(s);
+        } catch (NumberFormatException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     /**
      * 时间戳转换成时间
      * @param s 传入的时间戳
